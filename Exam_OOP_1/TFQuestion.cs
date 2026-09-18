ddd6a94f34a849f303a70302ec09192a043595fb
Ahmed El-dahy <ahmeddahy366-arch@gmail.com>
2026-09-18 15:59:04 +0300

    TFQuestion

---
+++ Exam_OOP_1/TFQuestion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam_OOP_1
+{
+    public class TFQuestion : Question, ICloneable
+    {
+        public TFQuestion(string header, string body, int mark, Answer[] answerList, Answer rightAnswers, Answer answerUser) : base(header, body, mark, answerList, rightAnswers, answerUser)
+        {
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+        public override string ToString()
+        {
+            return $"{Header} \n{Body} =>(Mark): {Mark}";
+        }
+    }
+}

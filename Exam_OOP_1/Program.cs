ff82ff3a4ccef98eb13513aceff7cdcf7a3f13c4
Ahmed El-dahy <ahmeddahy366-arch@gmail.com>
2026-09-18 16:07:21 +0300

    Program

--- Exam_OOP_1/Program.cs
+++ Exam_OOP_1/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace Exam_OOP_1
+﻿using System.Diagnostics;
+
+namespace Exam_OOP_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-         
+            Subject subject = new Subject(1, "Math");
+            subject.SubjectExam = subject.CreateExam();
+            Console.Clear();
+            Console.WriteLine("Do you want start exam(Y/N)");
+            bool Isvalid;
+            char choice;
+            do
+            {
+
+                Isvalid = char.TryParse(Console.ReadLine(), out choice) && (char.ToUpper(choice) == 'Y' || char.ToUpper(choice) == 'N');
+
+            } while (!Isvalid);
+
+            switch (choice)
+            {
+                case 'y':
+                    Stopwatch sw = new Stopwatch();
+                    sw.Start();
+                    subject.SubjectExam.ShowExam();
+                    sw.Stop();
+                    Console.WriteLine($"\n The  Time = {sw.Elapsed}");
+                    break;
+                case 'n':
+                    Console.WriteLine("Exam cancelled");
+
+                    break;
+
+            }
+
         }
     }
 }
+         
+ 

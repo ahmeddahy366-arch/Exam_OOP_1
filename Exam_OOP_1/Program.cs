04ba8ac5dd78a9b56a2ee03579751c8104f55b87
Ahmed El-dahy <ahmeddahy366-arch@gmail.com>
2026-09-18 10:53:39 +0300

    Add project files.

---
+++ Exam_OOP_1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exam_OOP_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
